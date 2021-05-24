400c953205fc70380c39d925a89627be448fa7c5
Dennis Nayandin <nayandindennis@yahoo.com>
2021-05-24 18:25:31 -0400

    modified text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Learning how to use Amazon Amplify
         </p>
         <a
           className="App-link"
