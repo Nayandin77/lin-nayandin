5c3178df6ad737a90f9c94f04add14b92110ec7f
Dennis Nayandin <nayandindennis@yahoo.com>
2021-05-25 14:57:15 -0400

    added json data for navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import menuLinksData from '../components/data/menu_links.json';
 
 const Header = () => {
 
     return (
-        <p>Test Text</p>
+        <div className="App-header">
+            <nav id="nav">
+                <div className="navbar">
+                    <ul>
+                        {
+                            menuLinksData.map((link) => 
+                                <li><a className={`icon ${link.class}`} href={link.href}><span>{link.text}</span></a></li>
+                            )
+                        }
+                        
+                        {/* <li><a className="icon projects" href="#projects"><span>Projects</span></a></li>
+                        <li><a className="icon hobbies" href="#hobbies"><span>Hobbies</span></a></li>
+                        <li><a className="icon contact" href="#contact"><span>Contact</span></a></li> */}
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        
     );
 }
 
