723d4a78fb1738b55f8cf3217191955ca8958ec3
Dennis Nayandin <nayandindennis@yahoo.com>
2021-06-10 17:04:33 -0400

    fixed Home css to fit variety of screen sizes

--- src/components/Home.js
+++ src/components/Home.js
@@ -68,7 +68,9 @@ const Home = () => {
     return (
         <div className="home-container">
             <div className="item-home-b"></div>
+
             <div className="item-home-a">
+
                 <div className="home-intro-text">
                     <p>Hello, my name is</p>
                 </div>
@@ -76,9 +78,11 @@ const Home = () => {
                     <p className="home-name-text-a">Dennis Nayandin</p>
                     <p className="home-name-text-b">(alias Validone)</p>
                 </div>
-                <div className="home-webpage-text">
+
+                <div className="home-description-text">
                     <p>I like the build things with Computers.</p>
                 </div>
+
                 <div className="home-paragraph-text">
                     <p>I'm a Virginia-based software engineer who's experienced in building and designing full-stack
                         web / mobile applications and application software. I'm CoFounder and CTO of my company 
@@ -86,15 +90,30 @@ const Home = () => {
                         building a variety of applications (web, mobile, software).
                     </p>
                 </div>
-                <div className="home-skills-list">
-                    <p>
-                        List of Skills
+
+                <div className="home-aboutme-container">
+                    <p className="home-aboutme-title">
+                        -- About Me ----
                     </p>
+                    <p className="home-aboutme-text">
+
+                    </p>
+                </div>
+
+                <div className="home-skills-list-section">
+                    <div className="home-skills-list-text">
+                        <p>
+                            List of Technologies that I'm skilled / experienced in:
+                        </p>
+                    </div>
+                    
                     { returnSkillsList() }
                 </div>
                 
             </div>
+
             <div className="item-home-c"></div>
+
         </div>
         
     );
