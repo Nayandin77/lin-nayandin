251a4b90e6edc2243cb729c8dab7ce9015720b4a
Dennis Nayandin <nayandindennis@yahoo.com>
2021-06-11 19:40:21 -0400

    starting Contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import './style/Layout.css';
+import './style/Contact.css';
 
 
 const Contact = () => {
 
     return (
         <div className="contact-container">
-            <div className="item-contact-a">
-                <p>Test Contact!</p>
-            </div>
-            <div className="item-contact-b">
-                <p>Test2</p>
-            </div>
-            <div className="item-contact-c">
-                <p>Test 3</p>
+            <div className="contact-title">
+                <p>Contact Me</p>
             </div>
         </div>
 
